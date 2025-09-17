c2b279e894de67849aa1c8805a6d9a73b12197c3
Mauro Antonio León Payano <mauroleonpayano@gmail.com>
2025-09-16 21:18:20 -0500

    Committing 2 items from workspace 102f0b15-25a9-45c2-aad6-e471d6c9a096

---
+++ ws_comercial/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,33 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "eb84d8bc-fba6-4586-946d-516e8da9b809",
+# META       "default_lakehouse_name": "L01_TDV_MUG",
+# META       "default_lakehouse_workspace_id": "102f0b15-25a9-45c2-aad6-e471d6c9a096",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "eb84d8bc-fba6-4586-946d-516e8da9b809"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
